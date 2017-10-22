e60da9078417049dbbbe4f5ddc04087acc906118
hirnschock <al.klein.de@googlemail.com>
2017-10-22 20:04:51 +0200

    E02 Part

---
+++ Top Down Shooter/Assets/Scripts/GunController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public Transform WeaponHold;
+    public Gun startingGun;
+    Gun equippedGun;
+
+    void Start()
+    {
+        if (startingGun != null)
+        {
+            EquipGun(startingGun);
+        }
+    }
+
+    public void EquipGun(Gun gunToEquip) {
+        if (equippedGun != null)
+        {
+            Destroy(equippedGun.gameObject);
+        }
+        equippedGun = Instantiate(gunToEquip,WeaponHold.position,WeaponHold.rotation) as Gun;
+        equippedGun.transform.parent = WeaponHold;
+    }
+}
